04dcea7f65e09bc03c20fac809cecab0066af5cd
Carden Z <ephemere@protonmail.com>
2018-12-28 02:17:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,8 +5,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  words = text.split(' ')
-	words.grep(/ \b(un)/ && /(ing)\b/)
+  # words = text.split(' ')
+	# words.grep(/ \b(un)/ && /(ing)\b/)
   text.scan(/un\w+ing/i)
 end
 
